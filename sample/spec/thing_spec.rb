c053c24aa0d0b20b0e1c59aae14f0fba75e6dc8c
Pistos <github.pistos@purepistos.net>
2011-08-08 17:21:27 -0400

    Shortened sample code.

--- sample/spec/thing_spec.rb
+++ sample/spec/thing_spec.rb
@@ -2,11 +2,8 @@ require_relative 'spec_helper'
 require 'thing'
 
 describe 'The thing under test' do
-  before :each do
-    SemiAutoTest.repeat_from_here
-  end
-
   it 'should sum integers' do
+    SemiAutoTest.repeat_from_here
     Thing.sum( 1, 1 ).should == 2
   end
 end
